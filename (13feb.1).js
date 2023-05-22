5a0bcbf0053bd5034a873df2866cf3829b4843d1
ANU <127592803+ANUJADAM@users.noreply.github.com>
2023-05-22 11:21:03 +0530

    Add files via upload

---
+++ (13feb.1).js
@@ -0,0 +1,102 @@
+// Hello world in Nodejs
+console.log(this)
+
+// Values and data types
+
+// 1.-permitive Data type
+
+// -numbers:2,3,4,4.5,24
+// string : "PWSKILLS" OR 'pwskills'
+// -Boolean : true , false
+// -Null
+// -Undefined
+
+// 2.Non-permitive data type
+// -Array
+
+let name1 = "hitesh"
+let name2 = "sushant"
+let name3 = "rohit"
+let name4 = "kaif"
+
+// Array declaration
+let name = ["hitesh","sushant","rohit","kaif",2,4,false,"amit"]  
+
+// -Object: {}  
+
+
+//  keyword = let ,const,var
+let myName = "Hitesh"
+
+myName = "Hitesh choudhary"
+
+// assignment operator
+let highScore = 300
+console.log(highScore);
+
+let pwSkillsCoursePrice = 3500
+console.log(pwSkillsCoursePrice);
+
+pwSkillsCoursePrice = 2000
+console.log(pwSkillsCoursePrice);
+
+let userName = "hitesh"
+console.log(userName);
+
+let pwCoursePrice = 200
+let gstOnCourse = 36
+let finalPurchaseAmount = pwCoursePrice + gstOnCourse
+console.log("Final amount to be paid :",finalPurchaseAmount);
+
+let = pwDSACourse = 500
+console.log("Final price of DSA course :", pwDSACourse + gstOnCourse);
+
+console.log("Final price of DSA course :", pwDSACourse - gstOnCourse);
+
+console.log("Final price of DSA course :", pwDSACourse * gstOnCourse);
+
+console.log("Final price of DSA course :", pwDSACourse / gstOnCourse);
+
+console.log("Answer is1 :" ,5+2);
+console.log("Answer is2 :" ,5-2);
+console.log("Answer is3 :" ,5*2);
+console.log("Answer is4 :" ,5/2);
+console.log("Answer is5 :" ,5%2);
+console.log("Answer is :" ,5**2);
+
+let hiteshHighScore = 200
+let rajuHighScore = 300
+// let rajuHighScore = "300"
+// console.log(hiteshHighScore === rajuHighScore);
+
+console.log(hiteshHighScore == rajuHighScore);
+console.log(hiteshHighScore > rajuHighScore);
+console.log(hiteshHighScore < rajuHighScore);
+console.log(hiteshHighScore != rajuHighScore);
+console.log(hiteshHighScore <= rajuHighScore);
+console.log(hiteshHighScore >= rajuHighScore);
+
+console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+let value1 = true
+let value2 = false
+let value3 = true
+let value4 = false
+
+console.log(value1 && value3);
+
+console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+let isLoggedIn = true
+let cardDetails = true
+console.log(isLoggedIn && cardDetails);
+
+
+// ||
+
+let gmailAccount = true
+let emailAccount = false
+let githubLogin = false
+console.log(gmailAccount || emailAccount || githubLogin);
+
+
