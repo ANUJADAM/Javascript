97d707df5e2b8e63495208aa2353095fe187a458
ANU <127592803+ANUJADAM@users.noreply.github.com>
2023-05-24 09:38:16 +0530

    Add files via upload

---
+++ (20feb.1).js
@@ -0,0 +1,59 @@
+let a = [1,2,3,4,"sanket",false]
+console.log(a);
+
+// let b = new Array();
+// console.log(b);
+
+// let b = new Array(5);
+// console.log(b);
+
+let b = new Array("abe","def","ghi");
+console.log(b);
+
+/*
+given array -> [11,21,31,41,51]
+index/Position->[0,1,2,3,4] last element index =aee.length-1
+*/
+
+console.log(a[0]);
+console.log(a[4]);
+
+a[4] = 5;
+console.log(a);
+
+console.log(a[14]);
+
+
+
+
+/*
+Arrays in JavaScript
+What is an Array?
+An array in JavaScript is a data structure that stores an ordered list of elements. It
+can hold elements of any data type, including numbers, strings, objects, and
+even other arrays. Arrays are a type of object in JavaScript and have a number of
+built-in methods for adding, removing, and manipulating elements.
+
+What do we need Arrays in JavaScript?
+● Grouping related data
+● Storing large amount of data
+● Improving performance
+● Ease of use
+● Better readability
+
+Declaration of Array
+1. Using square brackets [ ]
+2. Using an Array constructor
+3. Using an Array literal
+
+Array Indexing and Storing
+Indexing and storing in arrays are related concepts in JavaScript. The index of an
+element in an array determines its position within the array while storing in an
+array refers to the act of assigning a value to an element within the array.
+
+Accessing elements in an Array
+Accessing elements in an array in JavaScript is done using the square bracket
+notation and an index number. The index number represents the position of the
+element within the array, starting from 0 for the first element, 1 for the second
+element, and so on.
+*/
