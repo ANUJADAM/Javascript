91e4c2a673ad5a7bfa3f0e305eefa46ef129b680
ANU <127592803+ANUJADAM@users.noreply.github.com>
2023-05-24 09:08:31 +0530

    Add files via upload

---
+++ (14feb.1).js
@@ -0,0 +1,74 @@
+// Variable and values (numbers String,boolen etc)
+// operators
+// conditions and loops
+
+// let age = 19;
+
+// if(age >= 18){
+//     console.log("You are allowed");
+// }
+// else{
+//     console.log("You are not allowed");
+// }
+
+// let signal = "red";
+// if(signal == "red"){
+//     console.log("red => STOP")
+// }
+
+// else if(signal == "yellow"){
+//     console.log("Yellow => Go Slow")
+// }
+
+// elseif(signal == "yellow") {
+//  console.log("Green => fast");
+// }
+// else
+// {
+    // console.log("Invalid");
+// }
+// switch case
+// let user = "Admin";
+// switch(user){
+//     case "Admin":
+//         console.log("He is admin");
+//         break;
+
+//     case "student":
+//         console.log("He is student");
+//         break;
+
+//     case "Mentor":
+//         console.log("He is mentor");
+//         break;
+//     default:
+//         console.log("I am de faullt");
+// }
+
+
+// loops
+// do while,while,for
+// for(let i = 0; i < 5; i++)
+// {
+//     console.log(i);
+// }
+
+
+// while(init,condition,inc/dec)
+// let i = 0;
+// while(i < 5){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i = 0;
+// do{
+//   console.log("Hello World")
+// }while(i < 5);
+
+// Ternary operator/ Ternart Condition
+//  Condition ? True : False
+
+isloggedin = true;
+isloggedin ? console.log("Home page") : console.log("loginpage");
