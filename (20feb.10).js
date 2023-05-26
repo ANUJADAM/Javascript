7d2e18797f59d7cdd72f0b248e8b5091807bd558
ANU <127592803+ANUJADAM@users.noreply.github.com>
2023-05-26 14:48:19 +0530

    Add files via upload

---
+++ (20feb.10).js
@@ -0,0 +1,11 @@
+/*Self-invoking functions
+What is a self-invoking function?
+● A function that invokes itself immediately after its definition is referred to as a
+self-invoking (or self-executing) function.
+● A second set of parentheses follows the set of parentheses surrounding the
+anonymous function (which performs the execution).
+*/
+Syntax:
+(function (parameters) {
+ //body of the function
+})(arguments);
