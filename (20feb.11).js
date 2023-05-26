1ff5aa3fbc643011fc5a2bf478958cc2181b3bc6
ANU <127592803+ANUJADAM@users.noreply.github.com>
2023-05-26 14:56:50 +0530

    Add files via upload

---
+++ (20feb.11).js
@@ -0,0 +1,49 @@
+/*What was the need of objects
+List of content
+1. What are objects
+2. How are objects different from variables
+3. Why do we need object
+
+What are objects:
+
+● An object is an entity (properties and method) with a certain state and
+behavior. Examples include a car, pen, bicycle, chair, glass, keyboard, and
+monitor.
+● Objects can contain many values.
+● Object values are written as key : value pairs (key and value separated by a
+colon)
+
+Example for better understanding:
+
+                            Car (object)
+
+    Attributes/Data                                 Behaviour
+
+    Name,                                                Can start,
+    Manufacturer,                                        Can stop,
+    Fuel Capacity                                        Can accelerate
+
+
+Example for better understanding
+Name: Honda City, Manufacturer: Honda, Fuel capacity: 40 
+Name: Seltos, Manufacturer: Kia, Fuel capacity: 50 
+Variables                                         Objects
+A variable is a named stored location           Object is just a name given to user-defined variables
+which holds a value 
+A variable can be initialized in a usual        Objects are declared and initialized as per the developer’s wish
+manner.
+A variable can contain only one value           An object contains multiple values
+at a time 
+Eg. let name, var age, etc.                     Eg.let person = {firstName:"ABC", lastName:"XYZ", age:20}
+
+Why do we need objects:
+
+● Variables limit us to handling only one value at a time whereas objects enable
+us to have multiple values of multiple data types to be stored under one name.
+● This not only gives the privilege of escaping many variables for our
+applications but also organizes the code for better data handling.
+● Objects are a boon for developers to develop applications that we see in the
+current web world.
+● Objects give us the liberty to see and implement the scenarios in real-world
+form rather than disconnecting the actual application from the implemented
+one.*/
